cb00d41f74471599b726cb7bc6ac1e7b39f68a3d
Nurul Akbar Al-Ghifari <nurulakbaral@gmail.com>
2024-11-07 09:32:56 +0700

    Feat: Add metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react'
 import { Button } from '@mui/material'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <section>
-      <Button variant='contained'>Hello World!</Button>
+      <Head>
+        <title>Home</title>
+        <link rel='shortcut icon' href='https://assets.pokemon.com/static2/_ui/img/favicon.ico' />
+      </Head>
+
+      <Button variant='contained'>Hello World</Button>
     </section>
   )
 }
